5878ad8692271dc0ad1fe796df325fdab0c8dbd8
Sarah Lawrence <sarahlawrence1152@gmail.com>
2020-06-18 22:32:57 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+// import { rootReducer } from './reducers';
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+// STEP 1: CREATE A REDUX STORE
+const store = createStore(rootReducer);
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
